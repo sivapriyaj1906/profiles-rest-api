1a9d2a8f31585c15386fa9e293c1bfe492d2a81f
sivapriya1625 <jammala.sivapriya@gmail.com>
2025-01-06 11:17:00 +0530

    Added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,22 +1,34 @@
-from django.urls import path, include
+# from django.urls import path, include
 
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
-from profiles_api import views
+# from profiles_api import views
 
-router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-
-urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
-    path('', include(router.urls))
-]
-
+# router = DefaultRouter()
+# router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+# router.register('profile', views.UserProfileViewSet)
 
 # urlpatterns = [
 #     path('hello-view/', views.HelloApiView.as_view()),
+#     path('', include(router.urls))
 # ]
 
+
+# # urlpatterns = [
+# #     path('hello-view/', views.HelloApiView.as_view()),
+# # ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
 
 
 
